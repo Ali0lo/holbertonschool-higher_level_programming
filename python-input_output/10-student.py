79c4260e6c406f346336a1037d625200f51eab7b
Ali0lo <aliaze975@gmail.com>
2025-12-14 19:55:16 +0000

    gitpusher

---
+++ python-input_output/10-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Module that defines a Student class"""
+
+
+class Student: 
+    """A Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student with first_name, last_name and age
+
+        Args:
+            first_name (str): The first name of the student
+            last_name (str): The last name of the student
+            age (int): The age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance
+
+        Args:
+            attrs (list): List of attribute names to retrieve
+
+        Returns:
+            dict: Dictionary representation of the student
+        """
+        if attrs is None:
+            return self.__dict__
+        
+        result = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                result[attr] = getattr(self, attr)
+        return result
