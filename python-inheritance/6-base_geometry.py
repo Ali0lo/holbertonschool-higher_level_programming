bba53a27c0ce595e8016e6f35ba204ccabb4e971
Ali0lo <aliaze975@gmail.com>
2025-12-14 19:39:29 +0000

    gitpusher

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """A BaseGeometry class"""
+
+    def area(self):
+        """Raises an Exception with area() is not implemented message"""
+        raise Exception("area() is not implemented")
