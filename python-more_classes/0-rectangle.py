947d6b8d7e28260dfab22018a58699f56b4f2e97
Ali0lo <aliaze975@gmail.com>
2025-12-14 19:10:15 +0000

    gitpusher

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,2 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines a Rectangle class.
+"""
 class Rectangle:
+    """
+    This class defines a rectangle.
+    It doesn't have any methods or attributes yet.
+    """
     pass
