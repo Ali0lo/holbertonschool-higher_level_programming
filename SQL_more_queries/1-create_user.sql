a8f3607f332696ce6bffbdc8e568d13a7b0938fb
Ali0lo <aliaze975@gmail.com>
2026-01-08 19:08:50 +0000

    a

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Create the user user_0d_1 if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Flush privileges to apply the changes
+FLUSH PRIVILEGES;
