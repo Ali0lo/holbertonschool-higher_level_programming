25c74545f0a70a572db577e756bc52f90adcddf1
Ali0lo <aliaze975@gmail.com>
2026-01-08 19:22:26 +0000

    a

--- SQL_more_queries/9-cities_by_state_subquery.sql
+++ SQL_more_queries/9-cities_by_state_subquery.sql
@@ -1,5 +1,5 @@
--- List all cities with their corresponding state names
+-- List all cities with their corresponding state names in a single SELECT statement
 SELECT cities.id, cities.name, 
-       (SELECT name FROM states WHERE id = cities.state_id) AS state_name
+       (SELECT states.name FROM states WHERE states.id = cities.state_id) AS state_name
 FROM cities
 ORDER BY cities.id ASC;
